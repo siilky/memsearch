bb9bb914dd9c6fadac948128ed338d19fbd6d1d6
Ruslan <silkytail@gmail.com>
2024-10-25 11:50:36 +0300

    move function using logging to header

--- memsearch.cpp
+++ memsearch.cpp
@@ -1,10 +1,9 @@
 #include "pch.h"
 
 #include "memsearch.h"
-#include <Psapi.h>
 
 
-static std::tuple<LPVOID, size_t> GetModuleBaseAndSize(HMODULE hModule)
+std::tuple<LPVOID, size_t> GetModuleBaseAndSize(HMODULE hModule)
 {
     MEMORY_BASIC_INFORMATION mbi;
 
@@ -25,35 +24,4 @@ static std::tuple<LPVOID, size_t> GetModuleBaseAndSize(HMODULE hModule)
     auto size = (SIZE_T)((LPBYTE)mbi.BaseAddress + mbi.RegionSize - (LPBYTE)base);
 
     return std::make_tuple(base, size);
-}
-
-Lookup::Lookup(const wchar_t *module)
-{
-    MODULEINFO mi;
-    auto hModule = GetModuleHandleW(module);
-    if (hModule != NULL
-        && GetModuleInformation(GetCurrentProcess(), hModule, &mi, sizeof(mi)) != 0)
-    {
-        base_ = (byte*)mi.lpBaseOfDll;
-        baseLength_ = mi.SizeOfImage;
-
-        auto [base, size] = GetModuleBaseAndSize(hModule);
-        assert(base == base_);
-        assert(size == baseLength_);
-    }
-    else
-    {
-        //throw std::system_error(GetLastError(), std::system_category(), "GetModuleInformation failed");
-        var_debug(__func__, "GetModuleInformation failed with", GetLastError());
-    }
-
-    if (module != nullptr)
-    {
-        std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>, wchar_t> uconv;
-        var_debug(__func__, uconv.to_bytes(module), (void *)base_, ':', baseLength_);
-    }
-    else
-    {
-        var_debug(__func__, "executable", (void*)base_, ':', baseLength_);
-    }
 }
